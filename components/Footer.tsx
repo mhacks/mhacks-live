f580371a5471b052698c4db93bb2a00e4b183ad8
Brian Travis <bztravis88@gmail.com>
2023-11-13 23:43:21 -0500

    Add Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,25 @@
 import styles from './Footer.module.scss';
+import QuickLink from './QuickLink';
 
 export default function Footer() {
-  return <div className={styles.Footer}></div>;
+
+  const footerLinks = [
+    {
+      iconPath: '/instagram.svg',
+      text: '',
+      href: 'https://www.instagram.com/mhacks_/'
+    },
+    {
+      iconPath: '/x.svg',
+      text: '',
+      href: 'https://twitter.com/mhacks?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'
+    }
+  ];
+
+  return <div className={styles.Footer}>
+    <div className={styles.linkContainer}>
+      {footerLinks.map((quickLink) => <QuickLink key={quickLink.text} iconPath={quickLink.iconPath} text={quickLink.text} href={quickLink.href} color={'white'} />)}
+    </div>
+    <p>MHacks 16</p>
+  </div>;
 }
