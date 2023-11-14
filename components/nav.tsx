0c163f3bd5f0412d1b60e3b613919bf02443c53a
Brian Travis <bztravis88@gmail.com>
2023-11-14 11:06:49 -0500

    Add active link styling on Nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,38 +1,62 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './Nav.module.scss';
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import styleBuilder from '@/utils/styleBuilder'
+import styles from './Nav.module.scss'
 
 const links = [
   {
-    text: 'Home',
-    href: '/'
+    text: 'Schedule',
+    href: '/',
   },
   {
     text: 'Maps',
-    href: '/maps'
+    href: '/maps',
   },
   {
     text: 'Resources',
-    href: '/resources'
+    href: '/resources',
   },
   // {
   //   text: 'Prizes',
   //   href: '/prizes'
   // },
-];
+]
 
 export default function Nav() {
+
+  const pathname = usePathname()
+  console.log('pathname', pathname)
+
   return (
     <nav className={styles.nav}>
-      <Link href='/'>
-        <Image className={styles.logo} priority width={118} height={36} src='/images/logo.svg' alt="logo" />
-        <Image className={styles.logoMobile} priority width={36} height={36} src='/images/logoMobile.svg' alt="logo" />
+      <Link href="/">
+        <Image
+          className={styles.logo}
+          priority
+          width={118}
+          height={36}
+          src="/images/logo.svg"
+          alt="logo"
+        />
+        <Image
+          className={styles.logoMobile}
+          priority
+          width={36}
+          height={36}
+          src="/images/logoMobile.svg"
+          alt="logo"
+        />
       </Link>
       <div className={styles.links}>
-        {links.map((link) =>
-          <Link key={link.text} href={link.href}>{link.text}</Link>
-        )}
+        {links.map((link) => (
+          <Link key={link.text} className={styleBuilder([[styles.active, pathname===link.href]])} href={link.href}>
+            {link.text}
+          </Link>
+        ))}
       </div>
     </nav>
-  );
+  )
 }
