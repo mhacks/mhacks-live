81382dd49ee9e7997ab23d5d7db8c5400beb3e4e
Ecpi <28408948+Ecpii@users.noreply.github.com>
2025-09-13 15:49:54 -0400

    update quicklinks and hacking start time

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,21 +1,31 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import differenceInSeconds from 'date-fns/differenceInSeconds';
-import format from 'format-duration';
-import styleBuidler from '@/utils/styleBuilder';
-import styles from './Countdown.module.scss';
+import { useState, useEffect } from "react";
+import differenceInSeconds from "date-fns/differenceInSeconds";
+import format from "format-duration";
+import styleBuidler from "@/utils/styleBuilder";
+import styles from "./Countdown.module.scss";
 
 export default function Countdown() {
-  const [duration, setDuration] = useState<string>('');
+  const [duration, setDuration] = useState<string>("");
   const [diffInSec, setDiffInSec] = useState<number | null>(null);
 
-  const hackingStarts = new Date(2024, 8, 28, 11, 30);
-  const hackingEnds = new Date(2024, 8, 29, 12, 0);
-  const untilText = new Date().getTime() > hackingEnds.getTime() ? 'Hacking has ended.' : new Date().getTime() > hackingStarts.getTime() ? 'Until hacking ends!' : 'Until hacking begins!';
+  const hackingStarts = new Date("Sep 27, 2025 10:00:00");
+  const hackingEnds = new Date("Sep 28, 2025 10:00:00");
+  const untilText =
+    new Date().getTime() > hackingEnds.getTime()
+      ? "Hacking has ended."
+      : new Date().getTime() > hackingStarts.getTime()
+      ? "Until hacking ends!"
+      : "Until hacking begins!";
 
   const updateDuration = () => {
-    const destinationTime = new Date().getTime() > hackingEnds.getTime() ? new Date() : new Date().getTime() > hackingStarts.getTime() ? hackingEnds : hackingStarts;
+    const destinationTime =
+      new Date().getTime() > hackingEnds.getTime()
+        ? new Date()
+        : new Date().getTime() > hackingStarts.getTime()
+        ? hackingEnds
+        : hackingStarts;
 
     const diff = differenceInSeconds(destinationTime, Date.now()); // diff in seconds
     setDiffInSec(diff);
@@ -24,12 +34,14 @@ export default function Countdown() {
 
   useEffect(() => {
     updateDuration();
-    const interval = setInterval(() => { updateDuration(); }, 1000);
+    const interval = setInterval(() => {
+      updateDuration();
+    }, 1000);
 
     return () => {
       clearInterval(interval);
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const lessThanHour = (sec: number): boolean => {
@@ -38,7 +50,14 @@ export default function Countdown() {
 
   return (
     <div className={styles.countdown}>
-      <p className={styleBuidler([styles.numerials, [styles.pulse, diffInSec ? lessThanHour(diffInSec) : false]])}>{duration}</p>
+      <p
+        className={styleBuidler([
+          styles.numerials,
+          [styles.pulse, diffInSec ? lessThanHour(diffInSec) : false],
+        ])}
+      >
+        {duration}
+      </p>
       <p className={styles.until}>{untilText}</p>
     </div>
   );
--- components/QuickLink.tsx
+++ components/QuickLink.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from './QuickLink.module.scss';
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./QuickLink.module.scss";
 
 type QuickLinkProps = {
   iconPath: string;
@@ -9,7 +9,12 @@ type QuickLinkProps = {
   color: string;
 };
 
-export default function QuickLink({ iconPath, text, href, color }: QuickLinkProps) {
+export default function QuickLink({
+  iconPath,
+  text,
+  href,
+  color,
+}: QuickLinkProps) {
   return (
     <Link className={styles.container} href={href} target="_blank">
       <Image priority src={iconPath} width={48} height={48} alt="icon"></Image>
